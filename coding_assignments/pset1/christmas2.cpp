0637acebd16d22ebf0aaa6f146708958bc4e2ff5
valia-lab <73885976+valia-lab@users.noreply.github.com>
2021-11-26 21:20:43 +0200

    Add files via upload

--- coding_assignments/pset1/christmas2.cpp
+++ coding_assignments/pset1/christmas2.cpp
@@ -35,9 +35,10 @@ node *insert(node *last, int count, int x, int j){
             return newbie;
           }
           else if(last->parent!=NULL)
-               insert(last->parent, x, j);
+               insert(last->parent, ++count, x, j);
           else {            //if last = root
             //make newbie the new root,
+            node *newbie;
             newbie->left = last;
             newbie ->right = NULL;
             return NULL;       //last did not change
@@ -50,8 +51,12 @@ node *find(node *t, int j ,int key){
         if(t==NULL) return NULL;      //not found
         int v = t->val;
         if(v<key) return NULL;       // min heap
-        if (v == key)
-             return t;
+        if (v == key){
+             if(t->j==j)
+                 return t;
+             else return NULL;
+        }
+
         else{
           h = find(t->left, j, key);
           if((h->j)>0) return h;
@@ -62,7 +67,19 @@ node *find(node *t, int j ,int key){
 
 }
 
+int s(node *t, int j){
 
+        node *h;
+        if(t==NULL) return -1;      //not found
+        int v = t->val;
+        int jei = t->j;
+        if(jei==j)
+            return v;
+        else if(jei>j)
+            s(t->left, j);
+        else
+            s(t->right, j);
+}
 
 int main(int argc, char **argv){
 
@@ -82,10 +99,6 @@ int main(int argc, char **argv){
   }
   infile.close();
 
-  vector<vector<int>> *s = new vector<vector<int>>[K];
-
-
-
    //............build n cartesian trees in O(n*n).......................
   node **T = new node*[N];
   for(int i =1; i<K; i++){
@@ -94,7 +107,7 @@ int main(int argc, char **argv){
         temp = pref[j] - pref[i-1];
         delta = j-i+1;
         if(temp > K) break;             //i am interested only in s(i,j)<=K
-        last = insert(last, temp, j);
+        last = insert(last, 0, temp, j);
         if(last == NULL ) last = root;
       }
       T[i] = root;
@@ -108,15 +121,19 @@ int main(int argc, char **argv){
   for(int i=1; i<K; i++){
     for(int j = i+1; j<K+1; j++){
         c = 0;
-        temp = K-s[i][j];
-        for(int q = j+1; j<N; ){       //search for s(q,m): q>j
-            r = find(T[q], q, temp);
-            if(r!=NULL){
-                c = (r->j -q +1) + (j-i+1);        //total length
-                res = min(res, c);
-            }
-         }
+        if((temp = s(T[i],j))>0){
+            temp = K - temp;
+            for(int q = j+1; j<N; j++){       //search for s(q,m): q>j
+                r = find(T[q], q, temp);
+                if(r!=NULL){
+                    c = (r->j -q +1) + (j-i+1);        //total length
+                    res = min(res, c);
+                }
+             }
+        }
+        else break;                     //if s(i,j)>K then for every bigger j it exceeds K as well
     }
   }
+  cout<<res<<endl;
   return 0;
 }
