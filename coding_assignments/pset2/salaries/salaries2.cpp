f75e184b6840021be21abf2234c2797985a1bb03
valia-lab <73885976+valia-lab@users.noreply.github.com>
2022-01-05 19:32:11 +0200

    Add files via upload

--- coding_assignments/pset2/salaries/salaries2.cpp
+++ coding_assignments/pset2/salaries/salaries2.cpp
@@ -66,40 +66,34 @@ int find_lis(long long int a[], long int n, long long int *l){
 
     }
     return subseqs.size()-1;
+
 }
 
 int find_lds(long long int a[], long int n, long long int *r, int K){
 
+
       vector<int> subseqs;    // holds the last added element to each increasing subseq
       subseqs.push_back(-1); //dummy push to have vector index base = 1
       subseqs.push_back(a[n]);
-
-      if(a[n-1]<a[n] || a[n-1]-a[n]<K )
-          r[n] = 1;
-      else r[n] = 0;
       int back, maxi, dual, repeat = 0, flag;
       int pivot, from, to, j;
-      for(int i = n-1; i>0; i--){
+      if(a[n-1]<a[n])
+          r[n]=1;
+      else r[n] = 0;
+      for(int i =n-1; i>1; i--){
         //scan subseqs by decreasing size
         back = subseqs.size()-1 ;
         maxi = 1;
         dual = 0;
         repeat = 0;
         flag = 0;
-        //.........................
-        //  if(i ==n-3) exit(1);
-        printf("i is %d\n", i);
-
-        //........................
-
         //printf("%d %lld\n", subseqs[back], a[i]);
         if(subseqs[back] > a[i]){    //if a[i] etxends longest subseq
           subseqs.push_back(a[i]);
           flag = 1;
         }
 
-        else
-        {
+        else{
           from = 1;
           to = back-1;
 
@@ -121,14 +115,41 @@ int find_lds(long long int a[], long int n, long long int *r, int K){
 
            }
           /*............end of bsearch .....................................*/
-          if(dual)
-              r[i] = r[pivot];
-          else if(!maxi ){
+          if(!dual && !maxi ){
               j = to;
               subseqs[j+1] = a[i];
           }
-          else
+          else if(!dual)
             subseqs[1] = a[i];
+         }
+
+         ///......binary search in subseqs[] to find right most element
+         //.......that is compatible with a[i-1]......................
+         from = 1;
+         to = subseqs.size()-1;
+        //if(i == n-2)
+    /*    for(const auto& x: subseqs)
+              printf("%d ", x);
+           putchar('\n');*/
+
+         if(a[i]== subseqs[to] &&(a[i-1] <subseqs[to] || a[i-1]-subseqs[to]<K))
+             r[i] = to;
+         else{
+         repeat = 0;
+             while(from<=to && repeat<2){
+                //  printf("%d\n",i);
+                  pivot = from + (to-from)/2;
+                  //printf("%d\n",pivot);
+                  if((a[i-1]<subseqs[pivot]) || (a[i-1] - subseqs[pivot]<K)) {//compatible
+                      from = pivot+1;
+                      r[i] = pivot;
+                  }
+                  else to = pivot-1;
+                  if(from ==to)
+                     repeat++;
+            }
+          }
+
 
   /*  printf("after inserting a[ %d %s\n", i, "]");
       for(const auto& x: subseqs)
@@ -136,86 +157,68 @@ int find_lds(long long int a[], long int n, long long int *r, int K){
         putchar('\n');*/
 
        }
- }
-      return subseqs.size()-1;
+    return subseqs.size()-1;
   }
 
-int main(){
-  /*.............reading ...................*/
-  time(&start);
-  long int N, K;
-  scanf("%ld %ld", &N, &K);
+  int main(){
+    /*.............reading ...................*/
+    time(&start);
+    long int N, K;
+    scanf("%ld %ld", &N, &K);
 
-  long long int *a = new long long int[N+1];
-  long long int *l = new long long int[N+1];
-  long long int *r = new long long int[N+1];
-  a[0] = -1;
-  l[0] = -1;
-  r[0] = -1;
+    long long int *a = new long long int[N+1];
+    long long int *l = new long long int[N+1];
+    long long int *r = new long long int[N+1];
+    a[0] = -1;
+    l[0] = -1;
+    r[0] = -1;
 
-  for(long int i=1; i<N+1; i++){
-      scanf("%lld ", &a[i]);
+    for(long int i=1; i<N+1; i++){
+        scanf("%lld ", &a[i]);
+    }
+    getchar();
+
+  //
+  /*for(int i=1; i<N+1; i++){
+      printf("%lld ", a[i]);
   }
-  getchar();
+  putchar('\n');*/
+  //
 
-//
-/*for(int i=1; i<N+1; i++){
-    printf("%lld ", a[i]);
-}
-putchar('\n');*/
-//
+    int res ,len;
+    res = find_lis(a, N, l);
+    if(K>0 && res!=N){
+       find_lds(a, N, r, K);
+        //
+  /*    for(int i=1; i<N+1; i++)
+           cout<<" "<<l[i];
+        putchar('\n');
+        for(int i=1; i<N+1; i++)
+           cout<<" "<<r[i];
+        putchar('\n');*/
+        //
+        //check for compatible
+        int pivot , from, to, repeat;
+        for(int y = 1; y<N; y++){
+                len = l[y] + r[y+1];
+                if(len > res)
+                    res =len;
+        }
+    }
+    /*  printf("after inserting a[ %d %s\n", i, "]");
+        for(const auto& x: subseqs)
+        }
+     }
+    printf("%d\n", res);
+    //cout<<l[9]<<endl;
+  //  cout<<r[10]<<endl;*/
+    printf("%d\n", res);
+    time(&tend);
+    double time_taken = double(tend - start);
+    if(time_taken>1)
+        cout << "Time taken by program is : " << fixed  << time_taken << setprecision(5)<<endl;
 
-  int res ,len;
-  res = find_lis(a, N, l);
-  if(K>0 && res!=N){
-     find_lds(a, N, r, K);
-      //
-for(int i=1; i<N+1; i++)
-         cout<<" "<<l[i];
-      putchar('\n');
-      for(int i=1; i<N+1; i++)
-         cout<<" "<<r[i];
-      putchar('\n');
-      //
-      //check for compatible
-      int pivot , left, right, repeat;
-      for(int y = 1; y<N; y++){
-          if(a[y]<a[y+1] || (a[y]-a[y+1]<K)){ //if the start of left lis is small enough
-              len = l[y] + r[y+1];
-              if(len > res)
-                  res =len;
-          }
-          else{
-           ///......binary search in subseqs[] to find right most element
-           //.......that is compatible with a[i-1]......................
-           from = 1;
-           to = subseqs.size()-1;
-           while(from<to){
-             pivot = from + (to-from)/2;
-             if(i>1 && (a[y]<subseqs[pivot]) || (a[y] - subseqs[pivot]<K)) //compatible
-                 from = pivot;
-             else to = pivot;
-           }
-           r[y+1] = r[pivot];
-         }
-         len = l[y] + r[y+1];
-         if(len > res)
-             res =len;
-      }
-
-  /*  printf("after inserting a[ %d %s\n", i, "]");
-      for(const auto& x: subseqs)
-      }
-   }
-  printf("%d\n", res);
-  //cout<<l[9]<<endl;
-//  cout<<r[10]<<endl;*/
-  time(&tend);
-  double time_taken = double(tend - start);
-  if(time_taken>1)
-      cout << "Time taken by program is : " << fixed  << time_taken << setprecision(5)<<endl;
-
-  delete [] l;
-  delete [] r;
-  return 0;
-}
+    delete [] l;
+    delete [] r;
+    return 0;
+  }
